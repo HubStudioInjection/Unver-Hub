c4c742d10127ec694f2a69ea0da0fd81b4b83f3d
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-13 21:54:20 +0200

    Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet(""))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/HubStudioInjection/Unver-Hub/refs/heads/main/Library.lua"))()
 local Window = library:AddWindow("Preview", {
 		main_color = Color3.fromRGB(41, 74, 122),
 		min_size = Vector2.new(500, 600),
